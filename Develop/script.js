af33fb1bb6180ebb6f4421b2fecf10baad1646e9
Axel Estrada <axelestrada1890@gmail.com>
2023-04-05 15:03:20 -0700

    Breakthrough, getting the password to stop printing x's

--- Develop/script.js
+++ Develop/script.js
@@ -1,125 +1,52 @@
+"use strict";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+var lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let builtArray = [];
+// let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
 
 function generatePassword() {
 
-  var x = confirm("Do you want your password to contain lowercase letter?");
-  if (x = true) {
-    var x = confirm("Do you want your password to contain uppercase letters?");
-    if (x = true) {
-      var x = confirm("Do you want your password to contain numbers?");
-      if (x = true) {
-
-        function writePassword() {
-          var characters =
-            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          var password = "";
-          for (var i = 0; i < passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * passwordLength);
-            password += characters.substring(randomNumber, randomNumber + 1);
-          }
-        }
-        // let password = "";
-        // let passwordLength = prompt("How many characters do you want your password to have?");
-        // for (let i = 0; i < passwordLength; i++) {
-        //   password += password.substring(upperAlphabet, lowerAlphabet, numbers);
-        // }
-        // return password;
-        // function writePassword() {
-        //   var password = generatePassword();
-        //   var passwordText = document.querySelector("#password");
-
-        //   passwordText.value = password;
-
-      }
-    }
+  if (confirm("Do you want upper")) {
+    builtArray = [...upperAlphabet, ...builtArray];
   }
-}
-}
-  else if (x = false) {
-  var x = confirm("Do you want your password to contain uppercase letters?");
-  if (x = true) {
-    var x = confirm("Do you want your password to contain numbers?");
-    if (x = true) {
-      let password = "";
-      let passwordLength = prompt("How many characters do you want your password to have?");
-      for (let i = 0; i < passwordLength; i++) {
-        password += "";
-      }
-      return password;
-      function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-
-        passwordText.value = password;
-
-      }
-    }
+  if (confirm("Do you want lower")) {
+    builtArray = [...lowerAlphabet, ...builtArray];
   }
-}
-else if (x = false) {
-  var x = confirm("Do you want your password to contain numbers?");
-  if (x = true) {
-    let password = "";
-    let passwordLength = prompt("How many characters do you want your password to have?");
-    for (let i = 0; i < passwordLength; i++) {
-      password += "";
-    }
-    return password;
-    function writePassword() {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-
-      passwordText.value = password;
-
-    }
+  if (confirm("Do you want#")) {
+    builtArray = [...numbers, ...builtArray];
   }
-}
-else if ("") {
+  console.log(builtArray);
   let password = "";
-  let passwordLength = prompt("How");
+  let passwordLength = prompt("How long do you want your password?");
+
+
+
+
   for (let i = 0; i < passwordLength; i++) {
-    password += "";
+    let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
+    console.log(generatedLetterIndex);
+    console.log(builtArray[generatedLetterIndex])
+    password += builtArray[generatedLetterIndex];
   }
   return password;
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  }
 }
-  // if (confirm("Do you want your password to contain lowercse letters?")) {
-  //   if (confirm("Do you want your password to contain uppercase letters?")) {
-  //     if (confirm("Do you want your password to contain numbers?")) {
-
-  //     }
-  //   }
-  //   else {
-  //     confirm("Do you want your password to contain uppercase letters?");
-  //   }
-  // }
 
 
-  // let password = "";
-
-  // let passwordLength = prompt("How many characters do you want your password to have?")
-  // for (let i = 0; i < passwordLength; i++) {
 
 
-  //   password += "x";
-  // }
 
-  // return password;
-}
+
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
