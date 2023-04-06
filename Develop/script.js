8da065ab716a51a69363ea6b35fd77f5e3a382fb
Axel Estrada <axelestrada1890@gmail.com>
2023-04-06 05:30:05 -0700

    finished code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,26 +6,29 @@ var generateBtn = document.querySelector("#generate");
 var lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*']
 let builtArray = [];
-// let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
 
 function generatePassword() {
 
-  if (confirm("Do you want upper")) {
+  builtArray = [];
+  console.log(builtArray);
+
+  if (confirm("Include uppercase letters in your password?")) {
     builtArray = [...upperAlphabet, ...builtArray];
   }
-  if (confirm("Do you want lower")) {
+  if (confirm("Include lowercase letters in your password?")) {
     builtArray = [...lowerAlphabet, ...builtArray];
   }
-  if (confirm("Do you want#")) {
+  if (confirm("Include numbers in your passcode?")) {
     builtArray = [...numbers, ...builtArray];
+  }
+  if (confirm("Include special characters in your password?")) {
+    builtArray = [...specialChar, ...builtArray];
   }
   console.log(builtArray);
   let password = "";
   let passwordLength = prompt("How long do you want your password?");
-
-
-
 
   for (let i = 0; i < passwordLength; i++) {
     let generatedLetterIndex = Math.floor(Math.random() * builtArray.length);
@@ -35,17 +36,9 @@ function generatePassword() {
     console.log(builtArray[generatedLetterIndex])
     password += builtArray[generatedLetterIndex];
   }
+
   return password;
 }
-
-
-
-
-
-
-
-
-
 
 function writePassword() {
   var password = generatePassword();
